755b306f59469e675239b7c33f584c6a05c240f7
Kentaro Masukawa <masukawa.kentaro@classmethod.jp>
2025-01-08 00:14:25 +0900

    Initial commit

---
+++ test/step-functions-spring-batch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StepFunctionsSpringBatch from '../lib/step-functions-spring-batch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step-functions-spring-batch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StepFunctionsSpringBatch.StepFunctionsSpringBatchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
